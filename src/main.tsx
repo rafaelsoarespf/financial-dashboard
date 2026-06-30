34d661db9eb72771147ef883259a9553d72e8e68
Rafael Soares de Paula FIlho <rafaelsoares.p.f@gmail.com>
2026-06-30 02:57:09 -0300

    chore: organize project structure and configure Aurora CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+import '../vendor/aurora/reset.css'
+import '../vendor/aurora/globals.css'
+import '../vendor/aurora/classes.css'
+import '../vendor/aurora/themes.css'
+
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
